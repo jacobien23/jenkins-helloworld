cb8884f312a6556eee2b2aa3c7e328d91e512c0b
Jacobien <48165412+jacobien23@users.noreply.github.com>
2026-01-04 09:55:02 +0100

    Update greeting message to version 4

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Classe Devops version 3 !");
+        System.out.println("Hello, Classe Devops version 4 !");
     }
 }
 
